801c53c4a4b398953ee84cfe8d26e62b7b3cb8c0
AquilesDaniel <aquilesvdaniel@gmail.com>
2025-09-10 19:57:05 -0300

    fist commit

--- src/main/java/com/lucasmartins/mineiracaodados/controller/TurmaController.java
+++ src/main/java/com/lucasmartins/mineiracaodados/controller/TurmaController.java
@@ -1,14 +1,39 @@
 package com.lucasmartins.mineiracaodados.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/turmas")
 public class TurmaController {
 
-    @GetMapping("/turma")
+    @GetMapping
     public String buscarTurma() {
-        return "T04N - Mineração de dados";
+        return "T03N - Mineração de dados";
+    }
+
+    @PostMapping("/criar")
+    public String criarTurma() {
+        return "Turma T03N criada";
+    }
+
+    @PutMapping("/atualizar")
+    public String atualizarTurma() {
+        return "Turma T03N atualizada";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarTurma() {
+        return null;    
+    }
+
+    @GetMapping("/disciplina")
+    public String buscarDisciplina() {
+        return "[T03N] [Projeto] Mineiração de dados";
     }
 
 }
