68a7655f1ebd02ffa1d60d68d35783f8b8afb74d
izraelzz <izraelpicoli@gmail.com>
2025-09-10 11:14:32 -0300

    feat: adicionando API

---
+++ ISRAEL_ANGELO_REIS_PICOLI/com/israel/mineracao_de_dados/MineracaoDeDadosApplication.java
@@ -0,0 +1,13 @@
+package com.israel.mineracao_de_dados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MineracaoDeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MineracaoDeDadosApplication.class, args);
+	}
+
+}
