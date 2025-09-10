0dcd67cb7d820ca0b9242448c82b57a246aa07e9
Prept7K <felipeemanuelnegri@gmail.com>
2025-09-10 20:01:22 -0300

    first commit

--- src/main/java/com/lucasmartins/mineiracaodados/controller/TurmaController.java
+++ src/main/java/com/lucasmartins/mineiracaodados/controller/TurmaController.java
@@ -1,14 +1,30 @@
 package com.lucasmartins.mineiracaodados.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/turmas")
 public class TurmaController {
 
-    @GetMapping("/turma")
+    @GetMapping
     public String buscarTurma() {
-        return "T04N - Mineração de dados";
+        return "T03N - Mineração de dados";
+    }
+    @PostMapping("/atualizar")
+    public String atualizarTurma() {
+        return "Turma T03N atualizada";
+    }
+    @DeleteMapping("/delete")
+    public String deletarTurma() {
+        return null;
+    }
+    @GetMapping("/disciplina")
+    public String buscarDisciplina() {
+        return "T03N - [Projeto] Mineração de dados";
     }
 
 }
