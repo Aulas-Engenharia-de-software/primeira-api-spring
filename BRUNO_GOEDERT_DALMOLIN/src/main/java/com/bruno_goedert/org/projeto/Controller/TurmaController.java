d2a660c7dbcffb494ed2e6cb55c496d8ca6530e9
GoedertDalmolin <gbrunodalmolin@gmail.com>
2025-09-10 19:54:28 -0300

    Feat: added array

--- BRUNO_GOEDERT_DALMOLIN/src/main/java/com/bruno_goedert/org/projeto/Controller/TurmaController.java
+++ BRUNO_GOEDERT_DALMOLIN/src/main/java/com/bruno_goedert/org/projeto/Controller/TurmaController.java
@@ -3,6 +3,9 @@ package com.bruno_goedert.org.projeto.Controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/turmas")
 public class TurmaController {
@@ -36,5 +39,23 @@ public class TurmaController {
     @ResponseStatus(HttpStatus.OK)
     public String delete() {
         return "DELETE TUMAR 03";
+    }
+
+    @GetMapping("/disciplina")
+    @ResponseStatus(HttpStatus.OK)
+    public String getDisciplina() {
+        return "Mineiração de dados";
+    }
+
+    @GetMapping("/alunos")
+    @ResponseStatus(HttpStatus.OK)
+    public List<String> getAlunos() {
+        return Arrays.asList(
+                "Ana Silva",
+                "Carlos Souza",
+                "Mariana Oliveira",
+                "Pedro Santos",
+                "Beatriz Costa"
+        );
     }
 }
