17a5d3ff74c76b0cabacd9e150121b2fb9aeb472
gmmouraFag <gmmoura@mminha.fag.edu.br>
2025-09-23 20:59:26 -0300

    Adicionando arquivo do trabalho

--- src/main/java/com/lucasmartins/mineiracaodados/model/AlunoEntity.java
+++ src/main/java/com/lucasmartins/mineiracaodados/model/AlunoEntity.java
@@ -1,10 +1,8 @@
 package com.lucasmartins.mineiracaodados.model;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
 @Entity
 @Table(name = "aluno")
 public class AlunoEntity {
