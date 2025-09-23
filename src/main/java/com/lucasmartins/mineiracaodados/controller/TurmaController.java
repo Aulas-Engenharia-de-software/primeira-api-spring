94a9fe2122d9c9ed8c2e9335b280e31b26c7724c
gmmouraFag <gmmoura@mminha.fag.edu.br>
2025-09-22 23:13:45 -0300

    Fix: Corrigindo conflitos da branch

--- src/main/java/com/lucasmartins/mineiracaodados/controller/TurmaController.java
+++ src/main/java/com/lucasmartins/mineiracaodados/controller/TurmaController.java
@@ -2,47 +2,51 @@ package com.lucasmartins.mineiracaodados.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class TurmaController {
 
-    @PostMapping(value = "/api/nome", produces = "application/json")
-    public ResponseEntity<String> buscarAlunos() {
-        return ResponseEntity.status(HttpStatus.OK).body("Nome da turma: T03N");
+    @GetMapping(value = "/nome", produces = "application/json")
+    public ResponseEntity<String> buscarNomeTurma() {
+        return ResponseEntity.ok("Nome da turma: T03N");
     }
 
-    @PostMapping(value = "/api/quantidade", produces = "application/json")
+    @GetMapping(value = "/quantidade", produces = "application/json")
     public ResponseEntity<String> buscarQuantidadeAlunos() {
-        return ResponseEntity.status(HttpStatus.OK).body("Quantidade de alunos da turma T03N: 15");
+        return ResponseEntity.ok("Quantidade de alunos da turma T03N: 15");
     }
 
-    @PostMapping(value = "/api/criar-turma", produces = "application/json")
+    @PostMapping(value = "/criar-turma", produces = "application/json")
     public ResponseEntity<String> criarTurma() {
-        return ResponseEntity.status(HttpStatus.OK).body("Turma T03N criada");
+        return ResponseEntity.status(HttpStatus.CREATED).body("Turma T03N criada");
     }
 
-    @PostMapping(value = "/api/atualizar-turma", produces = "application/json")
+    @PutMapping(value = "/atualizar-turma", produces = "application/json")
     public ResponseEntity<String> atualizarTurma() {
-        return ResponseEntity.status(HttpStatus.OK).body("Turma T03N atualizada");
+        return ResponseEntity.ok("Turma T03N atualizada");
     }
 
-    @PostMapping(value = "/api/deletar-turma", produces = "application/json")
+    @DeleteMapping(value = "/deletar-turma", produces = "application/json")
     public ResponseEntity<String> deletarTurma() {
-        return ResponseEntity.status(HttpStatus.OK).body("");
+        return ResponseEntity.ok("Turma T03N deletada");
     }
 
-    @PostMapping(value = "/api/lista-alunos", produces = "application/json")
-    public ResponseEntity<String> listarAlunos() {
+    @GetMapping(value = "/lista-alunos", produces = "application/json")
+    public ResponseEntity<List<String>> listarAlunos() {
         List<String> alunos = new ArrayList<>();
         alunos.add("João");
         alunos.add("Maria");
         alunos.add("Pedro");
-
-        return ResponseEntity.ok("Lista de alunos: "+ alunos);
+        return ResponseEntity.ok(alunos);
     }
 
+    @PostMapping(value = "/turma", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<TurmaDto> criarOuEchoTurma(@RequestBody TurmaDto turma) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(turma);
+    }
 }
